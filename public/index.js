5ca69643e2c302f4f0db669b0f0258ecbd71921c
Michel Gawron <michel.gawron@devinci.fr>
2018-01-28 12:59:45 +0100

    feature(convargo form): Updating the code in order to add style elements feature(actors table): Editing javascript code in order to create a table with the actors

--- public/index.js
+++ public/index.js
@@ -2,36 +2,50 @@
 'use strict';
 
 (() => {
-  const render = (actors) => {
-    const fragment = document.createDocumentFragment();
-    const div = document.createElement('div');
-    const template = actors.map(actor => {
-      return `
-        <div class="actor">
-          <span>${actor.who}</span>
-          <span>${actor.type}</span>
-          <span>${actor.amount}</span>
-        </div>
+    const render = (actors) => {
+        const fragment = document.createDocumentFragment();
+
+        // Creating a table
+        const div = document.createElement('table');
+        div.className = "table";
+        const thead = document.createElement('thead');
+        thead.innerHTML = `
+        <tr>
+            <th>Actor</th>
+            <th>Type</th>
+            <th>Amount</th>
+        </tr>
+        `;
+        div.appendChild(thead);
+
+        // Mapping the elements to our table
+        const template = actors.map(actor => {
+            return `
+        <tr>
+          <td>${actor.who}</td>
+          <td>${actor.type}</td>
+          <td>${actor.amount} $</td>
+        </tr>
       `;
-    }).join('');
+        }).join('');
 
-    div.innerHTML = template;
-    fragment.appendChild(div);
-    document.querySelector('#actors').innerHTML = '';
-    document.querySelector('#actors').appendChild(fragment);
-  };
+        div.innerHTML += template;
+        fragment.appendChild(div);
+        document.querySelector('#actors').innerHTML = '';
+        document.querySelector('#actors').appendChild(fragment);
+    };
 
-  const button = document.querySelector('#compute');
+    const button = document.querySelector('#compute');
 
-  button.addEventListener('click', function onClick () {
-    const trucker = CONVARGO.getTrucker();
-    const distance = document.querySelector('#distance').value;
-    const volume = document.querySelector('#volume').value;
-    const option = document.querySelector('.option').checked;
-    const actors = CONVARGO.payActors(trucker, distance, volume, option);
+    button.addEventListener('click', function onClick() {
+        const trucker = CONVARGO.getTrucker();
+        const distance = document.querySelector('#distance').value;
+        const volume = document.querySelector('#volume').value;
+        const option = document.querySelector('.option').checked;
+        const actors = CONVARGO.payActors(trucker, distance, volume, option);
 
-    render(actors);
+        render(actors);
 
-    return;
-  });
+        return;
+    });
 })();
